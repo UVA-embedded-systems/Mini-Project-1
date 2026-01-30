594e97ac88219b7ee5e3174713f04cb2902e9a3e
Homa Alemzadeh <homa.alem@gmail.com>
2026-01-30 00:08:05 -0500

    Refactor PortE_Init function with consistent formatting

--- PORTE.c
+++ PORTE.c
@@ -9,6 +9,7 @@ void PortE_Init(void){
   GPIO_PORTE_AFSEL_R &= ~0x0F;    // disable alt funct on PE3-0
   GPIO_PORTE_DEN_R |= 0x0F;       // enable digital I/O on PE3-0
   GPIO_PORTE_PCTL_R = ~0x0000FFFF;
-  GPIO_PORTE_AMSEL_R &= ~0x0F;    // disable analog functionality on PE
+  GPIO_PORTE_AMSEL_R &= ~0x0F;    // disable analog functionality on PE3-0
 }
+
 
